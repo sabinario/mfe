75c9aef08e0cab852d94f492868bc400b1a835d0
Sabino Fernandez <83181231+sabinario@users.noreply.github.com>
2023-02-28 10:51:20 -0500

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
@@ -30,9 +29,6 @@ const devConfig = {
           requiredVersion: dependencies['react-dom'],
         },
       },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
